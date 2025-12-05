0a8e2a0a64eadb111408774b71e240230677714b
Tuğçe Karakuş <karakustugce1453@gmail.com>
2025-12-05 21:41:39 +0300

    feat: add basic image carousel structure with HTML/CSS for image carosel project

---
+++ image-carousel/script.js
@@ -0,0 +1,9 @@
+var index=0;
+function show_image(i){
+    index += i;
+    var images = document.getElementsByClassName("carousel-track");
+
+    var dots=document.getElementsByClassName("dot");
+    for(i=0;i<images.length;i++)
+        images[i]
+}
