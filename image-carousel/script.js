9b546bd44da56f965fc6ac248488b2dc84a2ee9d
Tuğçe Karakuş <karakustugce1453@gmail.com>
2025-12-11 01:41:34 +0300

    feat: finalize image carousel project

--- image-carousel/script.js
+++ image-carousel/script.js
@@ -1,9 +1,28 @@
-var index=0;
-function show_image(i){
-    index += i;
-    var images = document.getElementsByClassName("carousel-track");
+var index = 0;
+var dots = document.getElementsByClassName("dot");
+var images = document.getElementsByClassName("image");
 
-    var dots=document.getElementsByClassName("dot");
-    for(i=0;i<images.length;i++)
-        images[i]
+show_image(index);
+
+function show_image(step) {
+    index += step;
+
+    for (let i = 0; i < images.length; i++)
+        images[i].style.display = "none";
+
+    for (let i = 0; i < dots.length; i++)
+        dots[i].classList.remove("active");
+
+    if (index > images.length - 1) index = 0;
+    if (index < 0) index = images.length - 1;
+
+    images[index].style.display = "block";
+    dots[index].classList.add("active");
+}
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", () => {
+        index = i;
+        show_image(0);
+    });
 }
