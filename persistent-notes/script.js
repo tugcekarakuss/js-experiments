a0f631f81f58d26ade47dbfc4181143ac3c38e5b
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-01-16 22:16:35 +0300

    Complete basic notes app with add and delete functionality

--- persistent-notes/script.js
+++ persistent-notes/script.js
@@ -1,27 +1,29 @@
-const input=document.querySelector("#noteInput")
-const noteList=document.querySelector("#noteList")
-const addButton=document.querySelector("#addBtn")
-function addNote(){
-    if(input.value.length==0){
+const input = document.querySelector("#noteInput")
+const noteList = document.querySelector("#noteList")
+const addButton = document.querySelector("#addBtn")
+
+function addNote() {
+    if (input.value.length == 0) {
         alert("please add a note")
     }
-    else{
-       let note=input.value
-       const li=document.createElement("li")
-       const deleteBtn=document.createElement("button")
-       deleteBtn.innerText="delete"
-       li.append(note,deleteBtn)
-       noteList.appendChild(li)
-       input.value=""
+    else {
+        note = input.value
+        const li = document.createElement("li")
+        const deleteBtn = document.createElement("button")
+        deleteBtn.innerText = "delete"
+        li.append(note, deleteBtn)
+        noteList.appendChild(li)
 
-       deleteBtn.addEventListener("click",function(){
-        li.remove()
-       })
+        input.value = ""
+
+        deleteBtn.addEventListener("click", function () {
+            li.remove()
+        })
     }
 }
-input.addEventListener("keyup",function(event){
-    if(event.key=="Enter"){
+input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
         addNote()
     }
 })
-addButton.addEventListener("click",addNote)
+addButton.addEventListener("click", addNote)
